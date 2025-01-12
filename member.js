61fa8b5f52f8871b4830b7092265bd26f98498b0
☀️ <sumeya4hmed@gmail.com>
2025-01-12 17:24:25 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,17 @@
+function skillsMember(){
+
+    // function for the members' skills
+    var skills = {
+        'skills': {
+            'HTML': 9,
+            'CSS': 8,
+            'JS': 7,
+            'PHP': 5,
+            'Python': 6,
+            'Java': 4
+        }
+    };
+
+    // return the skills object
+    return skills;
+}
